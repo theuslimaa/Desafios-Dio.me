397822ae169a7ab20ed715cd9e9150e4c799898a
Matheus Lima <noutzinho@outlook.com>
2024-06-07 01:56:14 -0300

    Add files via upload

---
+++ niveldeheroidio.js
@@ -0,0 +1,28 @@
+let nomePerso = 'Homem Bimba'
+let xp = 10002
+
+if (xp < 1000) {
+    xp = 'Ferro'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp <= 2000) {
+    xp = 'Bronze'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp <= 5000) {
+    xp = 'Prata'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp <= 7000) {
+    xp = 'Ouro'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp <= 8000) {
+    xp = 'Platina'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp <= 9000) {
+    xp = 'Ascendente'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp <= 10000) {
+    xp = 'Imortal'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+} else if (xp >= 10001) {
+    xp = 'Radiante'
+    console.log('O heroi chamado ' + nomePerso + ' esta no nivel de heroi ' + xp);
+}
