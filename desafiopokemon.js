8c62d7057d4b5b8c11a24d34e58cb8484550410d
Matheus Lima <noutzinho@outlook.com>
2024-06-21 21:07:49 -0300

    Add files via upload

---
+++ desafiopokemon.js
@@ -0,0 +1,11 @@
+const statusPokemons = [
+    "Nome: POOCHYENA, " + "level: 2, " + "Sexo: Masculino, " + "Pontos de vida: 13: " + "Pokémon Cadastrado com sucesso",
+    "Nome: ZIGZAGOON, " + "level: 2, " + "Sexo: Feminino, " + "Pontos de vida: 13: " + "Pokémon Cadastrado com sucesso",
+    "Nome: DRAGONITE, " + "level: 5, " + "Sexo: Masculino, " + "Pontos de vida: 25: " + "Pokémon Cadastrado com sucesso",
+    "Nome: DRAGONITE, " + "level: 5, " + "Sexo: Feminino, " + "Pontos de vida: 24: " + "Pokémon Cadastrado com sucesso",
+    "Nome: DRAGONITE, " + "level: 5, " + "Sexo: Feminino, " + "Pontos de vida: 24: " + "Pokémon Cadastrado com sucesso",
+    "Nome: POOCHYENA, " + "level: 3, " + "Sexo: Feminino, " + "Pontos de vida: 15: " + "Pokémon Cadastrado com sucesso",
+    "Nome: WURMPLE, " + "level: 2, " + "Sexo: Masculino, " + "Pontos de vida: 14: " + "Pokémon Cadastrado com sucesso",
+]
+
+console.log(statusPokemons)
